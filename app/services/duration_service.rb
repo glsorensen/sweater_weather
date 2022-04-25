cb94a4627ffa96fcfa6aa5266101a5828c750486
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-25 11:14:45 -0600

    Feat: Duration Service and spec

---
+++ app/services/duration_service.rb
@@ -0,0 +1,18 @@
+require 'faraday'
+require 'json'
+
+class DurationService
+  def self.connection
+    Faraday.new("http://www.mapquestapi.com") do |faraday|
+      faraday.params['key'] = ENV['map_quest']
+    end
+  end
+
+  def self.get_duration(from, to)
+    response = connection.get("/directions/v2/route") do |faraday|
+      faraday.params['from'] = from
+      faraday.params['to'] = to
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
