b7c51dee74e99ccb7fe16fd40eadb79a0ccb2c95
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-25 11:15:49 -0600

    Feat: Refactor

--- spec/services/yelp_service_spec.rb
+++ spec/services/yelp_service_spec.rb
@@ -3,8 +3,7 @@ require 'rails_helper'
 RSpec.describe YelpService do
   it "gets restaurants for a given location" do
     service = YelpService.get_restaurants('pueblo,co', 'chinese')
-    binding.pry
     expect(service.class).to eq(Hash)
-    expect(service.count).to eq(1)
+    expect(service.count).to eq(3)
   end
 end
