9a78d0880f8ef3512b6ae43854428c467c2e538f
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-26 14:24:31 -0600

    Feat: road trip controller

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -3,14 +3,14 @@ module Api
     class RoadTripController < ApplicationController
       def create
         if params[:origin] && params[:destination] && params[:api_key]
-            trip_info = DurationFacade.duration(params[:origin], params[:destination])
-            if trip_info.nil?
-                render json: {error: 'please check that you have entered origin and destination'}
-            else
-                render json: RoadtripSerializer.new(trip_info)
-            end
+          trip_info = DurationFacade.duration(params[:origin], params[:destination])
+          if trip_info.nil?
+            render json: { error: 'please check that you have entered origin and destination' }
+          else
+            render json: RoadtripSerializer.new(trip_info)
+          end
         else
-            render json: {status: :unauthorized}
+          render json: { status: :unauthorized }
         end
       end
     end
