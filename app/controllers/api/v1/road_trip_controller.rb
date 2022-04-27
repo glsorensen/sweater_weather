a04f93633d5b472bec5efe5b60e3d744c2320314
Gunnar Sorensen <gunnar.l.sorensen@gmail.com>
2022-04-27 11:38:35 -0600

    Feat: Final Commit

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -7,7 +7,7 @@ module Api
         if params[:origin] && params[:destination] && params[:api_key]
           trip_info = DurationFacade.duration(params[:origin], params[:destination])
           if trip_info.nil?
-            render json: { error: 'please check that you have entered origin and destination' }
+            render json: { error: 'no route available' }
           else
             render json: RoadtripSerializer.new(trip_info)
           end
